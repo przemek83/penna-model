77fc4c1a771605d2f9b7da53d6170b3b6b5736b9
Przemek <4788832+przemek83@users.noreply.github.com>
2024-01-16 09:01:00 +0100

    Update penna.cpp
    Some improvements and cleaning.

--- penna.cpp
+++ penna.cpp
@@ -1,23 +1,22 @@
 #include "Common.h"
-#include "Individual.h"
 #include "NumbersGenerator.h"
 #include "Output.h"
 #include "Simulation.h"
 #include "SimulationData.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    float krok_symulacji =  // wielkosc kroku symulacji
-        abs(START_ODLOWOW - KONIEC_ODLOWOW) / (float)SYMULACJI_NA_PROCES;
-
-    Individual populacja[MAX_POP];
+    const float krok_symulacji =  // wielkosc kroku symulacji
+        abs(START_ODLOWOW - KONIEC_ODLOWOW) /
+        static_cast<float>(SYMULACJI_NA_PROCES);
 
     SimulationData simulationDataAvg;
     Output output(krok_symulacji, MAX_POP_LAT);
 
     NumbersGenerator generator;
 
-    for (int o = 1; o <= SYMULACJI_NA_PROCES + 1; o++)  // start kolejne symulacje
+    for (int o = 1; o <= SYMULACJI_NA_PROCES + 1;
+         o++)  // start kolejne symulacje
     {
         if (o == SYMULACJI_NA_PROCES + 1)
             output.otworz_pliki(0);
@@ -27,14 +26,14 @@ int main(int argc, char* argv[])
                 output.otworz_pliki2(o);
         }
 
-        clock_t start{clock()};
+        const clock_t start{clock()};
         Simulation simulation(o, krok_symulacji);
         simulation.run(output, generator, simulationDataAvg);
 
         output.zapisz_srednie(SYMULACJI_NA_PROCES, simulationDataAvg);
         output.zamknij_pliki(SYMULACJI_NA_PROCES + 1 - o);
 
-        clock_t koniec{clock()};
+        const clock_t koniec{clock()};
         if (o != SYMULACJI_NA_PROCES + 1)
         {
             printf(
