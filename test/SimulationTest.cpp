74015c35aa90e6f146def1eb93bccb3f7ed27610
Przemek <4788832+przemek83@users.noreply.github.com>
2024-01-18 12:29:11 +0100

    Update SimulationTest.cpp
    Changed order of 1 include.

--- test/SimulationTest.cpp
+++ test/SimulationTest.cpp
@@ -3,12 +3,12 @@
 
 #include <catch2/benchmark/catch_benchmark_all.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include "catch2/matchers/catch_matchers_string.hpp"
 
 #include "Generator.h"
 #include "Output.h"
 #include "Simulation.h"
 #include "SimulationData.h"
-#include "catch2/matchers/catch_matchers_string.hpp"
 
 namespace
 {
