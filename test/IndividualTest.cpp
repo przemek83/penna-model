d0810504dfc6c94ea113125a4b285b790f7b10d3
Przemek <4788832+przemek83@users.noreply.github.com>
2024-01-19 09:49:18 +0100

    Update IndividualTest.cpp
    Added more tests for offspring creation.

--- test/IndividualTest.cpp
+++ test/IndividualTest.cpp
@@ -16,45 +16,45 @@ TEST_CASE("Individual", "[penna]")
         individual.assignRandomBits(generator, 4);
         // 16384  = 00000000000000000100000000000000
         // 147464 = 00000000000000100100000000001000
-        REQUIRE(individual.ciag[0] == 16384);
-        REQUIRE(individual.ciag[1] == 147464);
+        REQUIRE(individual.genome_[0] == 16384);
+        REQUIRE(individual.genome_[1] == 147464);
     }
 
     Individual individual;
-    individual.ciag[0] = 16384;
-    individual.ciag[1] = 147464;
+    individual.genome_[0] = 16384;
+    individual.genome_[1] = 147464;
 
     SECTION("ageing passing 0")
     {
         individual.ageByOneYear(17);
-        REQUIRE(individual.ilosc_1 == 0);
+        REQUIRE(individual.survivedOnes_ == 0);
     }
 
     SECTION("ageing passing 1")
     {
         individual.ageByOneYear(18);
-        REQUIRE(individual.ilosc_1 == 1);
+        REQUIRE(individual.survivedOnes_ == 1);
     }
 
     SECTION("ageing passing 0 after 1")
     {
         individual.ageByOneYear(18);
         individual.ageByOneYear(19);
-        REQUIRE(individual.ilosc_1 == 1);
+        REQUIRE(individual.survivedOnes_ == 1);
     }
 
     SECTION("ageing passing multiple 1s")
     {
         individual.ageByOneYear(18);
         individual.ageByOneYear(47);
-        REQUIRE(individual.ilosc_1 == 2);
+        REQUIRE(individual.survivedOnes_ == 2);
     }
 
     SECTION("ageing passing all bits")
     {
         for (unsigned int i{0}; i < Config::bits_; ++i)
             individual.ageByOneYear(i);
-        REQUIRE(individual.ilosc_1 == 4);
+        REQUIRE(individual.survivedOnes_ == 4);
     }
 
     SECTION("bits as string")
@@ -63,5 +63,40 @@ TEST_CASE("Individual", "[penna]")
             individual.asBitString(),
             Catch::Matchers::Equals("000000000000000001000000000000000000000000"
                                     "0000100100000000001000"));
+    }
+
+    SECTION("offspring check bits")
+    {
+        MockedGenerator generator;
+        individual.assignRandomBits(generator, 4);
+        const std::string parentBits{individual.asBitString()};
+
+        const Individual child{individual.offspring()};
+
+        REQUIRE_THAT(child.asBitString(), Catch::Matchers::Equals(parentBits));
+    }
+
+    SECTION("offspring check age")
+    {
+        individual.age_ = 10;
+        const Individual child{individual.offspring()};
+
+        REQUIRE(child.age_ == 0);
+    }
+
+    SECTION("offspring ancestor")
+    {
+        individual.ancestor_ = 77;
+        const Individual child{individual.offspring()};
+
+        REQUIRE(child.ancestor_ == 77);
+    }
+
+    SECTION("offspring survived ones")
+    {
+        individual.survivedOnes_ = 3;
+        const Individual child{individual.offspring()};
+
+        REQUIRE(child.survivedOnes_ == 0);
     }
 }
