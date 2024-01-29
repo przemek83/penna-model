8db08982e00a19b215e583a49349657753b05b0d
Przemek <4788832+przemek83@users.noreply.github.com>
2024-01-29 16:03:15 +0100

    Update SimulationData.cpp
    Added missing casts.

--- SimulationData.cpp
+++ SimulationData.cpp
@@ -5,13 +5,14 @@ void SimulationAverages::integrateData(const SingleSimulationData& data)
 {
     for (std::size_t i{0}; i < years_; ++i)
     {
-        const SingleSimulationData::BasicData& basicData{data.getBasicData(i)};
+        const SingleSimulationData::BasicData& other{data.getBasicData(i)};
 
-        basicData_[i].families_ += basicData.families_;
-        basicData_[i].livingAtStart_ += basicData.livingAtStart_;
-        basicData_[i].births_ += basicData.births_;
-        basicData_[i].livingAtEnd_ += basicData.livingAtEnd_;
-        basicData_[i].deaths_ += basicData.deaths_;
+        basicData_[i].families_ += static_cast<float>(other.families_);
+        basicData_[i].livingAtStart_ +=
+            static_cast<float>(other.livingAtStart_);
+        basicData_[i].births_ += static_cast<float>(other.births_);
+        basicData_[i].livingAtEnd_ += static_cast<float>(other.livingAtEnd_);
+        basicData_[i].deaths_ += static_cast<float>(other.deaths_);
     }
 
     const std::array<float, Config::bits_>& deathsDistribution{
@@ -24,7 +25,7 @@ void SimulationAverages::integrateData(const SingleSimulationData& data)
     for (std::size_t i{0}; i < Config::bits_; i++)
     {
         bitsDistribution_[i] += bitsDistribution[i];
-        ageDistribution_[i] += ageDistribution[i];
+        ageDistribution_[i] += static_cast<float>(ageDistribution[i]);
         deathsDistribution_[i] += deathsDistribution[i];
     }
 
@@ -34,21 +35,22 @@ void SimulationAverages::integrateData(const SingleSimulationData& data)
 template <>
 void SimulationAverages::finalize()
 {
+    const float simulationsAsFloat{static_cast<float>(simulations_)};
     for (std::size_t v = 0; v < Config::bits_; v++)
     {
-        deathsDistribution_[v] /= simulations_;
-        bitsDistribution_[v] /= simulations_;
-        ageDistribution_[v] /= simulations_;
+        deathsDistribution_[v] /= simulationsAsFloat;
+        bitsDistribution_[v] /= simulationsAsFloat;
+        ageDistribution_[v] /= simulationsAsFloat;
     }
 
     for (std::size_t i{0}; i < years_; i++)
-        basicData_[i].families_ /= simulations_;
+        basicData_[i].families_ /= simulationsAsFloat;
 
     for (std::size_t i{0}; i < years_; i++)
     {
-        basicData_[i].livingAtStart_ /= simulations_;
-        basicData_[i].births_ /= simulations_;
-        basicData_[i].livingAtEnd_ /= simulations_;
-        basicData_[i].deaths_ /= simulations_;
+        basicData_[i].livingAtStart_ /= simulationsAsFloat;
+        basicData_[i].births_ /= simulationsAsFloat;
+        basicData_[i].livingAtEnd_ /= simulationsAsFloat;
+        basicData_[i].deaths_ /= simulationsAsFloat;
     }
 }
