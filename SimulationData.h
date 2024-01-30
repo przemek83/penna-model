40fae12617731975c870798d1cddfccdc617b223
Przemek <4788832+przemek83@users.noreply.github.com>
2024-01-30 14:26:14 +0100

    Removed SimulationData.cpp
    Removed SimulationData.cpp, moved code to template class header.

--- SimulationData.h
+++ SimulationData.h
@@ -22,10 +22,6 @@ public:
         basicData_.resize(years_);
     }
 
-    void integrateData(const SingleSimulationData& data);
-
-    void finalize();
-
     struct BasicData
     {
         T families_{0};
@@ -93,14 +89,76 @@ public:
         }
     }
 
+    void integrateData(const SingleSimulationData& data)
+    {
+        integrateBasicData(data);
+        integrateDistributions(data);
+        simulations_++;
+    }
+
+    void finalize()
+    {
+        finalizeBasicData();
+        finalizeDistributions();
+    }
+
 private:
-    void integrateBasicData(const SingleSimulationData& data);
+    void integrateBasicData(const SingleSimulationData& data)
+    {
+        for (std::size_t i{0}; i < years_; ++i)
+        {
+            const SingleSimulationData::BasicData& other{data.getBasicData(i)};
 
-    void integrateDistributions(const SingleSimulationData& data);
+            basicData_[i].families_ += static_cast<float>(other.families_);
+            basicData_[i].livingAtStart_ +=
+                static_cast<float>(other.livingAtStart_);
+            basicData_[i].births_ += static_cast<float>(other.births_);
+            basicData_[i].livingAtEnd_ +=
+                static_cast<float>(other.livingAtEnd_);
+            basicData_[i].deaths_ += static_cast<float>(other.deaths_);
+        }
+    }
 
-    void finalizeBasicData();
+    void integrateDistributions(const SingleSimulationData& data)
+    {
+        const std::vector<float>& deathsDistribution{
+            data.getDeathsDistribution()};
+        const std::vector<float>& bitsDistribution{data.getBitsDistribution()};
+        const std::vector<int>& ageDistribution{data.getAgeDistribution()};
 
-    void finalizeDistributions();
+        for (std::size_t i{0}; i < Config::bits_; i++)
+        {
+            bitsDistribution_[i] += bitsDistribution[i];
+            ageDistribution_[i] += static_cast<float>(ageDistribution[i]);
+            deathsDistribution_[i] += deathsDistribution[i];
+        }
+    }
+
+    void finalizeBasicData()
+    {
+        const float simulationsAsFloat{static_cast<float>(simulations_)};
+        for (std::size_t i{0}; i < years_; i++)
+            basicData_[i].families_ /= simulationsAsFloat;
+
+        for (std::size_t i{0}; i < years_; i++)
+        {
+            basicData_[i].livingAtStart_ /= simulationsAsFloat;
+            basicData_[i].births_ /= simulationsAsFloat;
+            basicData_[i].livingAtEnd_ /= simulationsAsFloat;
+            basicData_[i].deaths_ /= simulationsAsFloat;
+        }
+    }
+
+    void finalizeDistributions()
+    {
+        const float simulationsAsFloat{static_cast<float>(simulations_)};
+        for (std::size_t v = 0; v < Config::bits_; v++)
+        {
+            deathsDistribution_[v] /= simulationsAsFloat;
+            bitsDistribution_[v] /= simulationsAsFloat;
+            ageDistribution_[v] /= simulationsAsFloat;
+        }
+    }
 
     std::vector<float> deathsDistribution_;
     std::vector<float> bitsDistribution_;
