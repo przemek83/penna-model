c5f230eaaced77e4a99eb4aa197b9b33c6c04f9c
Przemek <4788832+przemek83@users.noreply.github.com>
2024-02-24 18:00:24 +0100

    Update Config.cpp
    Enhanced error msgs.

--- src/Config.cpp
+++ src/Config.cpp
@@ -32,11 +32,12 @@ std::map<Field, std::string> fieldToString{
     {Field::SIMULATIONS, "simulations"},
 };
 
-std::string createErrorMsg(Field field, const std::string& condition)
+std::string createErrorMsg(Field field, const std::string& condition,
+                           int currentValue)
 {
     std::ostringstream os;
     os << "check failed: " << fieldToString[field] << " " << condition
-       << std::endl;
+       << ", got " << currentValue << std::endl;
     return os.str();
 }
 }  // namespace
@@ -83,58 +84,71 @@ Config::Params loadConfig(std::istream& configFile)
 
 bool isValid(const Params& params)
 {
-    std::string errorString;
+    std::string errorMsg;
 
     if (params.maxPopulation_ <= 0)
-        errorString += createErrorMsg(Field::MAX_POPULATION, "> 0");
+        errorMsg +=
+            createErrorMsg(Field::MAX_POPULATION, "> 0", params.maxPopulation_);
 
     if (params.years_ <= 0)
-        errorString += createErrorMsg(Field::YEARS, "> 0");
+        errorMsg += createErrorMsg(Field::YEARS, "> 0", params.years_);
 
     if (params.livesOnStart_ <= 0)
-        errorString += createErrorMsg(Field::LIVES_ON_START, "> 0");
+        errorMsg +=
+            createErrorMsg(Field::LIVES_ON_START, "> 0", params.livesOnStart_);
 
     if (params.mutationsDelta_ < 0)
-        errorString += createErrorMsg(Field::MUTATIONS_DELTA, ">= 0");
+        errorMsg += createErrorMsg(Field::MUTATIONS_DELTA, ">= 0",
+                                   params.mutationsDelta_);
 
     if (params.maxMutations_ < 0)
-        errorString += createErrorMsg(Field::MAX_MUTATIONS, ">= 0");
+        errorMsg +=
+            createErrorMsg(Field::MAX_MUTATIONS, ">= 0", params.maxMutations_);
 
     if (params.startingMutations_ < 0)
-        errorString += createErrorMsg(Field::STARTING_MUTATIONS, ">= 0");
+        errorMsg += createErrorMsg(Field::STARTING_MUTATIONS, ">= 0",
+                                   params.startingMutations_);
 
     if (params.reproductionAge_ < 0)
-        errorString += createErrorMsg(Field::REPRODUCTION_AGE, ">= 0");
+        errorMsg += createErrorMsg(Field::REPRODUCTION_AGE, ">= 0",
+                                   params.reproductionAge_);
 
     if (params.offspringCount_ < 0)
-        errorString += createErrorMsg(Field::OFFSPRING_COUNT, ">= 0");
+        errorMsg += createErrorMsg(Field::OFFSPRING_COUNT, ">= 0",
+                                   params.offspringCount_);
 
     if (params.chanceForOffspring_ < 0)
-        errorString += createErrorMsg(Field::OFFSPRING_CHANCE, ">= 0");
+        errorMsg += createErrorMsg(Field::OFFSPRING_CHANCE, ">= 0",
+                                   params.chanceForOffspring_);
 
     if (params.chanceForOffspring_ > 100)
-        errorString += createErrorMsg(Field::OFFSPRING_CHANCE, "<= 100");
+        errorMsg += createErrorMsg(Field::OFFSPRING_CHANCE, "<= 100",
+                                   params.chanceForOffspring_);
 
     if (params.simulationsCount_ < 1)
-        errorString += createErrorMsg(Field::SIMULATIONS, ">= 1");
+        errorMsg += createErrorMsg(Field::SIMULATIONS, ">= 1",
+                                   params.simulationsCount_);
 
     if (params.startingMutations_ > Params::bits_)
-        errorString += createErrorMsg(Field::STARTING_MUTATIONS,
-                                      "<= " + std::to_string(Params::bits_));
+        errorMsg += createErrorMsg(Field::STARTING_MUTATIONS,
+                                   "<= " + std::to_string(Params::bits_),
+                                   params.startingMutations_);
 
     if (params.reproductionAge_ > Params::bits_)
-        errorString += createErrorMsg(Field::REPRODUCTION_AGE,
-                                      "<= " + std::to_string(Params::bits_));
+        errorMsg += createErrorMsg(Field::REPRODUCTION_AGE,
+                                   "<= " + std::to_string(Params::bits_),
+                                   params.reproductionAge_);
 
     if (params.livesOnStart_ > params.maxPopulation_)
-        errorString += createErrorMsg(
-            Field::LIVES_ON_START,
-            "lover than " + fieldToString[Field::MAX_POPULATION]);
+        errorMsg +=
+            createErrorMsg(Field::LIVES_ON_START,
+                           "lover than " + fieldToString[Field::MAX_POPULATION],
+                           params.livesOnStart_);
 
-    if (errorString.empty())
+    if (errorMsg.empty())
         return true;
 
-    std::cerr << "Configuration is invalid:" << std::endl << errorString;
+    std::cerr << "Configuration is invalid:" << std::endl << errorMsg;
 
     return false;
 }
