b3fcefc494ad332af96b25bc5475c933bd366059
Przemek <4788832+przemek83@users.noreply.github.com>
2024-02-26 22:01:31 +0100

    Update Timer.cpp
    Remove not needed new line.

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -7,7 +7,6 @@ Timer::~Timer()
     const auto end{std::chrono::steady_clock::now()};
     const auto timeElapsed{end - start_};
     std::cout
-        << std::endl
         << "Execution time: "
         << std::chrono::duration_cast<std::chrono::seconds>(timeElapsed).count()
         << "s." << std::endl;
