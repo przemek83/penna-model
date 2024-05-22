7fbeb320ca7857162a42a3228316d712d554accd
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-05-22 11:17:32 +0200

    Generate all basic plots.

--- plots/generate_basic_plots.py
+++ plots/generate_basic_plots.py
@@ -6,13 +6,14 @@ print(os.getcwd())
 os.chdir("basic")
 print(os.getcwd())
 
-result = subprocess.run("gnuplots -V", shell=True)
-
+result = subprocess.run("gnuplot -V", shell=True)
 try:
     result.check_returncode()
 except subprocess.CalledProcessError:
     print("Problem when calling 'gnuplot' command. Gnuplot not available?")
     sys.exit(1)
 
-gnuplot_cmd = 'gnuplot -c population.plt'
-subprocess.run(gnuplot_cmd, shell=True)
+for path in os.listdir("."):
+    if path.endswith(".plt"):
+        gnuplot_cmd = 'gnuplot -c ' + path
+        subprocess.run(gnuplot_cmd, shell=True)
