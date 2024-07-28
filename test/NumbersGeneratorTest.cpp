82ff6145d9e55f48e08f6667c539c90e5fad4705
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-07-28 19:24:44 +0200

    Add tests for NumberGenerator class.

---
+++ test/NumbersGeneratorTest.cpp
@@ -0,0 +1,42 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#include <src/NumbersGenerator.h>
+
+TEST_CASE("NumbersGenerator", "[penna]")
+{
+    const int seed{7};
+    NumbersGenerator generator{8, seed};
+
+    SECTION("Get bit position")
+    {
+        for (auto i : {0, 1, 6, 2, 3, 7, 5, 3})
+            REQUIRE(generator.getBitPosition() == i);
+    }
+
+    SECTION("Get percent chance")
+    {
+        NumbersGenerator generator{8, seed};
+        for (auto i : {7, 22, 78, 32, 44, 98, 73, 46})
+            REQUIRE(generator.getPercentChance() == i);
+    }
+
+    SECTION("Get int")
+    {
+        for (auto i : {7630, 22734, 77992, 31897, 43841, 97823, 72347, 45558})
+            REQUIRE(generator.getInt(0, 100'000) == i);
+    }
+
+    SECTION("Get int for 0 generator")
+    {
+        for (auto i : {0, 0, 0})
+            REQUIRE(generator.getInt(0, 0) == i);
+    }
+
+    SECTION("Get int for single nmumber generator")
+    {
+        const int expected{66};
+        for (auto i : {expected, expected, expected})
+            REQUIRE(generator.getInt(expected, expected) == i);
+    }
+}
