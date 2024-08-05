d905bd8c02273949611e0332bb923521de5c1d66
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-08-05 22:16:08 +0200

    Some small improvements.

--- test/FakeGenerator.cpp
+++ test/FakeGenerator.cpp
@@ -1,11 +1,11 @@
 #include "FakeGenerator.h"
 
 FakeGenerator::FakeGenerator(std::list<int> numbers)
-    : Generator{0}, numbers_{numbers}
+    : Generator{0}, numbers_{std::move(numbers)}
 {
 }
 
-int FakeGenerator::getInt(int low, int high)
+int FakeGenerator::getInt([[maybe_unused]] int low, [[maybe_unused]] int high)
 {
     const int number{numbers_.front()};
     numbers_.pop_front();
