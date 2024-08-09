2a75f64341791d7a8b6970485f19753d2c901197
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-08-09 15:57:09 +0200

    Split ProgressBar  derivatives tests into 2 files.

---
+++ test/ProgressBarOverallTest.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <sstream>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <src/Config.h>
+#include <src/ProgressBarOverall.h>
+
+TEST_CASE("Overall Progress Callback")
+{
+    const int years{1000};
+    config::Params params;
+    params.simulationsCount_ = 1;
+    params.years_ = years;
+
+    const int simId{0};
+    ProgressBarOverall progressBar{params.years_, params.simulationsCount_};
+    const int length{progressBar.getLength()};
+    const int firstYearWithMarker{(years / length) - 1};
+
+    std::streambuf* oldCoutBuffer{std::cout.rdbuf()};
+    std::ostringstream output;
+    std::cout.rdbuf(output.rdbuf());
+    output.str("");
+
+    SECTION("start")
+    {
+        progressBar.update(0, simId);
+        REQUIRE(output.str() == "[");
+    }
+
+    SECTION("before progress")
+    {
+        progressBar.update(firstYearWithMarker - 1, simId);
+        REQUIRE(output.str() == "");
+    }
+
+    SECTION("progress")
+    {
+        progressBar.update(firstYearWithMarker, simId);
+        REQUIRE(output.str() == "*");
+    }
+
+    SECTION("after progress")
+    {
+        progressBar.update(firstYearWithMarker + 1, simId);
+        REQUIRE(output.str() == "");
+    }
+
+    SECTION("end")
+    {
+        const int lastYear{years - 1};
+        progressBar.update(lastYear, simId);
+        REQUIRE(output.str() == "*]\n");
+    }
+
+    std::cout.rdbuf(oldCoutBuffer);
+}
--- test/ProgressBarSequentialTest.cpp
+++ test/ProgressBarSequentialTest.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <memory>
 #include <sstream>
 
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers.hpp>
 
 #include <src/Config.h>
-#include <src/ProgressBarOverall.h>
 #include <src/ProgressBarSequential.h>
 
 TEST_CASE("Sequential Progress Callback")
@@ -31,57 +28,6 @@ TEST_CASE("Sequential Progress Callback")
         const std::string expectedOutput{
             "1/" + std::to_string(params.simulationsCount_) + " ["};
         REQUIRE(output.str() == expectedOutput);
-    }
-
-    SECTION("before progress")
-    {
-        progressBar.update(firstYearWithMarker - 1, simId);
-        REQUIRE(output.str() == "");
-    }
-
-    SECTION("progress")
-    {
-        progressBar.update(firstYearWithMarker, simId);
-        REQUIRE(output.str() == "*");
-    }
-
-    SECTION("after progress")
-    {
-        progressBar.update(firstYearWithMarker + 1, simId);
-        REQUIRE(output.str() == "");
-    }
-
-    SECTION("end")
-    {
-        const int lastYear{years - 1};
-        progressBar.update(lastYear, simId);
-        REQUIRE(output.str() == "*]\n");
-    }
-
-    std::cout.rdbuf(oldCoutBuffer);
-}
-
-TEST_CASE("Overall Progress Callback")
-{
-    const int years{1000};
-    config::Params params;
-    params.simulationsCount_ = 1;
-    params.years_ = years;
-
-    const int simId{0};
-    ProgressBarOverall progressBar{params.years_, params.simulationsCount_};
-    const int length{progressBar.getLength()};
-    const int firstYearWithMarker{(years / length) - 1};
-
-    std::streambuf* oldCoutBuffer{std::cout.rdbuf()};
-    std::ostringstream output;
-    std::cout.rdbuf(output.rdbuf());
-    output.str("");
-
-    SECTION("start")
-    {
-        progressBar.update(0, simId);
-        REQUIRE(output.str() == "[");
     }
 
     SECTION("before progress")
